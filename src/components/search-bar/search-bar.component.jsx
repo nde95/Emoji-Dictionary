245bcf2384a6f53412da9e2efb6f837f6072fb2d
nde95 <ndennison268@gmail.com>
2023-05-25 12:43:21 -0400

    search bar set to handle results on render

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -1,13 +1,22 @@
+import React, { useState } from 'react';
 import "./search-bar.styles.css"
 
-const SearchBar = () => {
-    return (
-        <div className="search-container">
-            <div className="emoji-search">
-                <input type="text" placeholder="Search" />
-            </div>
-        </div>
-    )
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+    onSearch(newSearchTerm);
+  };
+
+  return (
+    <div className="search-container">
+      <div className="emoji-search">
+        <input type="text" placeholder="Search" value={searchTerm} onChange={handleInputChange} />
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
