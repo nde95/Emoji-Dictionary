2f44af0337e65cc22478f9471560e97a813c283e
nde95 <ndennison268@gmail.com>
2023-05-25 12:42:21 -0400

    conditional rendering set on page load and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ndeLogo from '/nde.svg'
 import EmojisList from './components/emoijs list/emojis-list.component'
 import SearchBar from './components/search-bar/search-bar.component'
+import EmojiSearch from './components/emoji-search/emoji-search.component'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('');
+  const [initialLoad, setInitialLoad] = useState(true);
+
+  useEffect(() => {
+    // Update initialLoad state after the first render
+    setInitialLoad(false);
+  }, []);
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
 
   return (
     <>
-    <SearchBar />
-    <EmojisList />
+      <SearchBar onSearch={handleSearch} />
+      {initialLoad || searchTerm === '' ? <EmojisList /> : <EmojiSearch searchTerm={searchTerm} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
