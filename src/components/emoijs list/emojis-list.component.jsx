10af3fcd90a7b94f93778b84cf3dec8401d1306a
nde95 <ndennison268@gmail.com>
2023-05-25 12:54:08 -0400

    copy to clipboard on click

--- src/components/emoijs list/emojis-list.component.jsx
+++ src/components/emoijs list/emojis-list.component.jsx
@@ -1,29 +1,34 @@
-import emojisList from "../emojis.json"
-import "./emojis-list.styles.css"
+import emojisList from "../emojis.json";
+import { useState } from "react";
+import "./emojis-list.styles.css";
 
 const EmojisList = () => {
-    const emojis = getRandomEmojis(12);
+  const emojis = getRandomEmojis(12);
 
-    function getRandomEmojis(count) {
-        const shuffledEmojis = emojisList.sort(() => 0.5 - Math.random());
-        return shuffledEmojis.slice(0, count);
-    }
+  function getRandomEmojis(count) {
+    const shuffledEmojis = emojisList.sort(() => 0.5 - Math.random());
+    return shuffledEmojis.slice(0, count);
+  }
 
-    return (
-        <div className="emoji-grid">
-          {emojis.map((emoji, index) => (
-            <div key={index} className="emoji-card">
-              <div className="emoji-item">
-                <div className="emoji-symbol">{emoji.symbol}</div>
-                <div className="emoji-title">{emoji.title}</div>
-              </div>
-            </div>
-          ))}
+  const handleCopyToClipboard = (emojiSymbol) => {
+    navigator.clipboard.writeText(emojiSymbol);
+    // You can also show a notification or perform any other actions after copying
+    console.log("Copied to clipboard:", emojiSymbol);
+  };
+
+  return (
+    <div className="emoji-grid">
+      {emojis.map((emoji, index) => (
+        <div key={index} className="emoji-card">
+          <div className="emoji-item" onClick={() => handleCopyToClipboard(emoji.symbol)}>
+            <div className="emoji-symbol">{emoji.symbol}</div>
+            <div className="emoji-title">{emoji.title}</div>
+          </div>
         </div>
-      );
-    };
-
-
+      ))}
+    </div>
+  );
+};
 
 export default EmojisList;
 
